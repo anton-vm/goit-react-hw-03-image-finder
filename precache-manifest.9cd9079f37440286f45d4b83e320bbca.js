fb1bc321c2225c4a1c7e18c68d3d434256cd3015
anton-vm <anton_vm@ukr.net>
2020-04-26 14:14:23 +0300

    Updates

---
+++ precache-manifest.9cd9079f37440286f45d4b83e320bbca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb921b30d920232189ef9d4060eae05d",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "84caf0e0f7b942b49723",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.59b2ea93.chunk.css"
+  },
+  {
+    "revision": "a37ec381eac0e97306ec",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.83f33678.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.83f33678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84caf0e0f7b942b49723",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.05dddf8d.chunk.js"
+  },
+  {
+    "revision": "ef7f04d3f12739069562",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.79cd0c33.js"
+  }
+]);
